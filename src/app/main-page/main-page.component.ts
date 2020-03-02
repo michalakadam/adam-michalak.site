ad80222e3c2bb7ddd69f3a072a45f2b6976dafcf
Adam Michalak <adam.michalak.dev@gmail.com>
2020-03-02 08:55:12 +0100

    add animated text on main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
+import { trigger, state, animate, transition, style } from '@angular/animations';
+
+export const fadeOut = trigger('fadeOut', [
+  state('active', style({ opacity: 1 })),
+  state('inactive', style({ opacity: 0 })),
+  transition('* => inactive', [animate(1495)])
+]);
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  animations: [fadeOut],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
+ 
+  private description = "self-taught";
+  subscription: Subscription;
+  private invocationNumber = 0;
+  private descriptionState = "active";
 
   constructor() { }
 
   ngOnInit() {
+    const source = interval(1500);
+    this.subscription = source.subscribe(() => this.pickDescription());
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private pickDescription(): void {
+    if (this.invocationNumber % 3 === 0) {
+      this.description = "highly-driven";
+    } else if (this.invocationNumber % 3 === 1) {
+      this.description =  "continously growing";
+    } else {
+      this.description = "self-taught";
+    }
+    this.invocationNumber++;
+  }
+
+  onDone($event: any) {
+    this.descriptionState = this.descriptionState === 'active' ? 'inactive' : 'active';
+  }
 }
