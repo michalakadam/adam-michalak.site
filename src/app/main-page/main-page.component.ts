c7448a5162786b9dfb1a0352f6134df1afdadd56
Adam Michalak <adam.michalak.dev@gmail.com>
2020-03-02 09:52:53 +0100

    remove rxjs interval to improve animation synchronicity

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
 import { trigger, state, animate, transition, style } from '@angular/animations';
 
 export const fadeOut = trigger('fadeOut', [
   state('active', style({ opacity: 1 })),
   state('inactive', style({ opacity: 0 })),
-  transition('* => inactive', [animate(1500)])
+  transition('* => inactive', [animate(1750)])
 ]);
 
 @Component({
@@ -15,23 +14,13 @@ export const fadeOut = trigger('fadeOut', [
   styleUrls: ['./main-page.component.scss'],
   animations: [fadeOut],
 })
-export class MainPageComponent implements OnInit, OnDestroy {
+export class MainPageComponent {
  
   private description = "self-taught";
-  subscription: Subscription;
   private invocationNumber = 0;
   private state = "active";
 
   constructor() { }
-
-  ngOnInit() {
-    const source = interval(1500);
-    this.subscription = source.subscribe(() => this.pickDescription());
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-  }
 
   private pickDescription(): void {
     if (this.invocationNumber % 3 === 0) {
@@ -45,6 +34,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
   }
 
   onDone($event: any) {
+    this.pickDescription();
     this.state = this.state === 'active' ? 'inactive' : 'active';
   }
 }
