ef1da418a3e3320ab454c11f8f247dd9a8696ec7
Adam Michalak <adam.michalak.dev@gmail.com>
2020-03-14 14:15:11 +0100

    add content and background to 'about' page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-  constructor() { }
+export class AboutComponent implements OnInit, OnDestroy {
+  resizeObservable$: Observable<Event>;
+  resizeSubscription$: Subscription;
+  isMobileScreen: boolean;
 
   ngOnInit() {
+      this.isMobileScreen = this.computeIsMobileScreen(window.innerWidth);
+
+      this.resizeObservable$ = fromEvent(window, 'resize');
+      this.resizeSubscription$ = this.resizeObservable$.subscribe( event => {
+        this.isMobileScreen = this.computeIsMobileScreen(event.target.innerWidth);
+      });
   }
 
+  private computeIsMobileScreen(screenWidth: number) {
+    console.log(screenWidth);
+    return screenWidth < 961 ? true : false;
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe()
+  }
 }
