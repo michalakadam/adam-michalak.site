79dfdcda7448ee215d422470d1390a138376e213
Adam Michalak <adam.michalak.dev@gmail.com>
2020-03-21 14:42:48 +0100

    import article data from json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { ArticleCardComponent } from './articles-list/article-card/article-card.
     AppRoutingModule,
     MatCardModule,
     MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
