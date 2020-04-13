cae2505704f1ab52170faf64a11c1377220e41d5
Adam Michalak <adam.michalak.dev@gmail.com>
2020-04-13 08:33:12 +0200

    add empty component with article on automation with scripts

---
+++ src/app/articles/generic-article/content-templates/automation-with-scripts/automation-with-scripts.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'automation-with-scripts',
+    templateUrl: './automation-with-scripts.html',
+    styleUrls: ['../article-shared-styles.scss'],
+})
+export class AutomationWithScriptsComponent { }
--- src/app/articles/generic-article/content-templates/content-templates.module.ts
+++ src/app/articles/generic-article/content-templates/content-templates.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GithubCodeReviewsComponent } from './github-code-reviews/github-code-reviews';
+import { AutomationWithScriptsComponent } from './automation-with-scripts/automation-with-scripts';
 
 
 @NgModule({
   declarations: [
     GithubCodeReviewsComponent,
+    AutomationWithScriptsComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     GithubCodeReviewsComponent,
+    AutomationWithScriptsComponent,
   ]
 })
 export class ContentTemplatesModule { }
