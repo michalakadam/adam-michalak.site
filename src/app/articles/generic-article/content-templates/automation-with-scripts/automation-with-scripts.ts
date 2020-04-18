5fcbc343c7bc952253645045bcdb77971e1b1ad7
Adam Michalak <adam.michalak.dev@gmail.com>
2020-04-18 13:26:38 +0200

    move image container style to shared stylesheet

--- src/app/articles/generic-article/content-templates/automation-with-scripts/automation-with-scripts.ts
+++ src/app/articles/generic-article/content-templates/automation-with-scripts/automation-with-scripts.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'automation-with-scripts',
     templateUrl: './automation-with-scripts.html',
-    styleUrls: ['../article-shared-styles.scss', './automation-with-scripts.scss'],
+    styleUrls: ['../article-shared-styles.scss'],
 })
 export class AutomationWithScriptsComponent { }
