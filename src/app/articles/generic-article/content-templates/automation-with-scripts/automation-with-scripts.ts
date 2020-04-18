7e5589dcac42d4948ebeabbc64320ce84217ac6b
Adam Michalak <adam.michalak.dev@gmail.com>
2020-04-18 10:52:00 +0200

    add article on scripts

--- src/app/articles/generic-article/content-templates/automation-with-scripts/automation-with-scripts.ts
+++ src/app/articles/generic-article/content-templates/automation-with-scripts/automation-with-scripts.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'automation-with-scripts',
     templateUrl: './automation-with-scripts.html',
-    styleUrls: ['../article-shared-styles.scss'],
+    styleUrls: ['../article-shared-styles.scss', './automation-with-scripts.scss'],
 })
 export class AutomationWithScriptsComponent { }
