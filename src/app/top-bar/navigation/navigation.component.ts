6d619395e5b49904cc210f18811e925dc6601210
Adam Michalak <adam.michalak.dev@gmail.com>
2025-09-06 19:14:11 +0200

    fix: router link active

--- src/app/top-bar/navigation/navigation.component.ts
+++ src/app/top-bar/navigation/navigation.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatButton],
 })
 export class NavigationComponent {}
